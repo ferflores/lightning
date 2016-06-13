3a0b4555bd6a17bbbb0c7153c75ecd32ac77dcea
ferflores <ferflores@gmail.com>
2016-06-13 09:37:25 -0500

    map default style

--- src/client/modules/map.js
+++ src/client/modules/map.js
@@ -1,6 +1,28 @@
+var regionStyle = {
+  initial: {
+    fill: '#000000',
+    "fill-opacity": 1,
+    stroke: 'none',
+    "stroke-width": 0,
+    "stroke-opacity": 1
+  },
+  hover: {
+    "fill-opacity": 0.8,
+    cursor: 'pointer'
+  },
+  selected: {
+    fill: 'yellow'
+  }
+}
+
 var map = {
   init: function(){
-    $('#map').vectorMap({map: 'world_mill'});
+    $('#map').vectorMap(
+      {
+        map: 'world_mill',
+        backgroundColor:'#5f5f5f',
+        regionStyle: regionStyle
+      });
     }
 }
 
